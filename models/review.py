5e356a51961f232f51beee70b4be4ac992e200f1
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-19 13:24:29 -0700

    v2

--- models/review.py
+++ models/review.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This is the review class"""
+
+"""The review based model class"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
 
 class Review(BaseModel, Base):
     """This is the class for Review
@@ -11,6 +12,7 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
+    
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
