94d505ef89f3bf7b0575d347a589661860d63615
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-19 12:30:58 -0700

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,8 @@
 -- these password for hbnb_test was be setted to hbnb_test_pwd
 -- hbnb_test has all the privilegs on the database
 -- hbnb_test had SELECTED privilegea on the database
--- hbnb_test_db or the user hbnb_test already exists, your script 
+-- hbnb_test_db or the user hbnb_test already exists, your script
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
