2861bf7fc9545445dbb17e41faf03edfe283cbb0
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-19 12:30:27 -0700

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,9 @@
--- Script that prepares a MySQL server for the project:
+-- this scripted prepared the MySQL server for this project:
 -- A database hbnb_test_db, A new user hbnb_test (in localhost)
+-- these password for hbnb_test was be setted to hbnb_test_pwd
+-- hbnb_test has all the privilegs on the database
+-- hbnb_test had SELECTED privilegea on the database
+-- hbnb_test_db or the user hbnb_test already exists, your script 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
