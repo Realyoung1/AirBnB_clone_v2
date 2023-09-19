56b493f7bf252fe7ff2d7b3c2cad6cee12c71dd9
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-19 12:32:38 -0700

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,6 +6,7 @@
 -- hbnb_test_db or the user hbnb_test already exists, your script
 
 
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
