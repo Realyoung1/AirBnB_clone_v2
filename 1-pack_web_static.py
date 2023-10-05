72346bd6e6196df6be799d01a597d2434295ea1c
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-05 10:24:08 -0700

    v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from shlex import split
 
 env.user = 'ubuntu'
-env.hosts = ['34.74.70.205', '34.224.30.177']
+env.hosts = ['52.91.148.167', '52.86.158.155']
 
 
 def do_pack():
