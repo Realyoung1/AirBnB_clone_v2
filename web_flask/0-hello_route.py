a102e242867e9a88d4bb3afcee84173d1e3bb1ca
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-19 02:56:25 -0700

    v2 airbnb

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Started a Flask web application with these scripts
+    the web apps was listed on 0.0.0.0, port 5000
+    with routes /: display “Hello HBNB!”
+    in my route def option strict_slashes=False was used
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """Say hello to the client"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
