e2a59e0c53591bcc32dbaa04a7d1671ca5bb36c8
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-11-18 02:51:33 -0800

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,14 +5,14 @@
     with routes /: display “Hello HBNB!”
     in my route def option strict_slashes=False was used
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     """Say hello to the client"""
-    return 'Hello HBNB!'
+    return render_template("100-hbnb.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
